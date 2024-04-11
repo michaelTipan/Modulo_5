34634a8a6258f138748ea08a84ac1b96c1f9381f
michaelTipan <michaeltipan99@gmail.com>
2024-04-10 21:06:57 -0500

    Reto 7 POO --> OK

--- POO/reto6.py
+++ POO/reto6.py
@@ -28,6 +28,35 @@ class Auto:
         else:
             print("¡Ya déjame descansar por favor!")
 
+    @classmethod
+    def auto_nuevo(cls):
+        return cls("Toyota", "Fortuner", 2024)
+
+    @staticmethod
+    def comparar_kilometraje(auto1, auto2):
+        if auto1.kilometraje == auto2.kilometraje:
+            print("Tienen el mismo kilometraje")
+        else:
+            print("Tienen diferente kilmetraje")
+
+    @staticmethod
+    def es_auto_nuevo(auto):
+        if auto.año == 2024 and auto.kilometraje == 0:
+            print("El auto es nuevo")
+        else:
+            print("El auto es usado")
+
+    @classmethod
+    def auto_usado(cls):
+        marca = "Chevrolet"
+        modelo = "Spark"
+        año = "2020"
+        auto = cls(marca, modelo, año)
+        auto.actualizar_kilometraje(35000)
+        return auto
+
+
+
 mi_auto = Auto("Toyota", "Corolla", 2020)
 mi_auto.mostrar_informacion()
 mi_auto.actualizar_kilometraje(15000)
@@ -35,4 +64,13 @@ mi_auto.actualizar_kilometraje(10000)
 mi_auto.realizar_viaje(5000)
 mi_auto.realizar_viaje(-200)
 mi_auto.estado_auto()
+
+auto1 = Auto.auto_nuevo()
+auto1.mostrar_informacion()
+Auto.es_auto_nuevo(auto1)
+auto2 = Auto.auto_usado()
+auto2.mostrar_informacion()
+Auto.es_auto_nuevo(auto2)
+Auto.comparar_kilometraje(auto1, auto2)
+
 
